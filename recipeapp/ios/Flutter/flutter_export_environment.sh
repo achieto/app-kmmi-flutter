7faa75244f0e1a2ea6f6311d527cc537005c7d50
achieto <anggie.t17210@gmail.com>
2021-09-07 15:24:48 +0700

    project

--- recipeapp/ios/Flutter/flutter_export_environment.sh
+++ recipeapp/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\user\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\projects\recipeapp"
+export "FLUTTER_ROOT=C:\Users\AR\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Back\AAAAAAA\KMMI\recipeapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
